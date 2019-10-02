22d5c3d49170773543b8ef46638b2ecfa9942842
Aziz Faozi <admin@labseni.com>
2019-10-02 14:20:45 +0700

    nir

---
+++ client.cpp
@@ -0,0 +1,52 @@
+#include "client.h"
+#include "accdb.h"
+Client::Client(QObject *parent, qintptr handle) : QObject(parent), QRunnable ()
+{
+    this->handle = handle;
+}
+
+void Client::run()
+{
+
+    qInfo() << this << " run " << QThread::currentThread();
+
+    QTcpSocket* socket = new QTcpSocket(nullptr);
+    if(!socket->setSocketDescriptor(handle))
+    {
+	qInfo()<<"Sokect nya error";
+        qCritical() << socket->errorString();
+        delete socket;
+        return;
+    }
+
+
+
+
+    /* waitForReadyRead */
+    socket->waitForReadyRead();
+
+
+    QByteArray request = socket->readAll();
+    qInfo() << "Request Length: " << request.length();
+    qInfo() <<request;
+    accdb AksesDB;
+    AksesDB.write_db(request);
+
+
+    /* Daemon */
+    QByteArray data("kalistaumari");
+    QByteArray response;
+
+
+    response.append(data);
+    socket->write(response);
+    socket->waitForBytesWritten();
+   
+    //socket->waitForDisconnected(6000);
+    socket->close();
+    socket->deleteLater();
+
+    qInfo() << this << " done " << QThread::currentThread();
+
+
+}
---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include <QCoreApplication>
+#include "server.h"
+#include "accdb.h"
+
+int main(int argc, char *argv[])
+{
+
+    /*
+        username : trumonuser
+        password : trumonpassword
+    */
+    QCoreApplication a(argc, argv);
+    QString hostname;
+    QString username;
+    QString password;
+    QString database;
+    Server server;
+    //server.start(2020);
+    if (argc == 5){
+        hostname = argv[1];
+        username = argv[2];
+        password = argv[3];
+        database = argv[4];
+        accdb aksesD(hostname, username, password, database);
+        
+        server.start(6969);
+    }
+    else{
+        qInfo()<<"Cara menggunakannya ";
+        qInfo()<<"./NeiraRecvServ [hosname] [username] [password] [database]";
+    }
+
+
+
+    return a.exec();
+}
